8944788aa0d115c6c5de157f9c9f0415a8af1fbb
moss1232 <saito-to@belong.co.jp>
2022-10-22 20:58:57 +0900

    feat: display context for input

--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
 	fmt.Println("ヒットならy, コールならnを押してください")
-	d := newDeck()
-	p := NewPlayer()
-	p.hit(*d)
-	fmt.Printf("%v\n", p.hand)
-	fmt.Printf("%v", d)
+	//d := newDeck()
+	//p := NewPlayer()
+	input := bufio.NewScanner(os.Stdin)
+	input.Scan()
+	fmt.Printf("%+v", input.Text())
+	//p.hit(*d)
+	//fmt.Printf("%v\n", p.hand)
+	//fmt.Printf("%v", d)
 }
