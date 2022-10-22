1cabb21b139d1a9c466111502dac954dac0ad591
moss1232 <saito-to@belong.co.jp>
2022-10-22 22:04:59 +0900

    feat: set up for game

--- main.go
+++ main.go
@@ -1,18 +1,46 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 )
 
-func main() {
+func msgHitOrCall() {
 	fmt.Println("ヒットならy, コールならnを押してください")
-	//d := newDeck()
-	//p := NewPlayer()
-	input := bufio.NewScanner(os.Stdin)
-	input.Scan()
-	fmt.Printf("%+v", input.Text())
+}
+
+func setup(p, dl *player, dc *deck) {
+	p.hit(*dc)
+	dl.hit(*dc)
+	pretty(p)
+	pretty(dl)
+}
+
+func pretty(p *player) {
+	fmt.Printf("name:%s\n", p.name)
+	for _, h := range p.hand {
+		fmt.Println("+-+")
+		fmt.Printf("|%s|\n", h.number)
+		fmt.Print("+-+\n\n")
+	}
+}
+
+func main() {
+	dc := newDeck()
+	fmt.Printf("%v", dc)
+	p := NewPlayer("player")
+	dl := NewPlayer("dealer")
+	setup(p, dl, dc)
+	//msgHitOrCall()
+	//input := bufio.NewScanner(os.Stdin)
+	//input.Scan()
+	//it := input.Text()
+	//if it == "Y" {
+	//	p.hit(*dc)
+	//}
+	//if it == "n" {
+	//
+	//}
+	//fmt.Printf("%+v", input.Text())
 	//p.hit(*d)
 	//fmt.Printf("%v\n", p.hand)
 	//fmt.Printf("%v", d)
--- player.go
+++ player.go
@@ -1,11 +1,13 @@
 package main
 
 type player struct {
+	name string
 	hand []card
 }
 
-func NewPlayer() *player {
+func NewPlayer(name string) *player {
 	return &player{
+		name: name,
 		hand: []card{},
 	}
 }
@@ -16,4 +18,5 @@ func (p *player) stand() {
 
 func (p *player) hit(d deck) {
 	p.hand = append(p.hand, d[0])
+	d = append(d[:0], d[1:]...)
 }
